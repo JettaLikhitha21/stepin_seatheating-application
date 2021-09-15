698c5b14fa7ce8fb4c83405bd353d456ea9d983d
JettaLikhitha21 <89720767+JettaLikhitha21@users.noreply.github.com>
2021-09-15 10:43:06 +0530

    Update activity3.h

--- 3_Implementation/inc/activity3.h
+++ 3_Implementation/inc/activity3.h
@@ -2,23 +2,44 @@
 #define __ACTIVITY_3_H__
 /**
  * @file activity3.h
- * @author anoop
- * @brief activity3
+ * @author jettalikhitha
+ * @brief Header file for activity3
  * @version 0.1
- * @date 2021-04-27
+ * @date 2021-09-14
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+
+/**
+ * Macro Definitions
+ */
+#define PWM_20_PERCENT (205) /**< Output PWM for 20% duty cycle */
+#define PWM_40_PERCENT (410) /**< Output PWM for 40% duty cycle */
+#define PWM_70_PERCENT (717) /**< Output PWM for 70% duty cycle */
+#define PWM_95_PERCENT (973) /**< Output PWM for 95% duty cycle */
+
+/**
+ * Include files
+ */ 
+#include <util/delay.h>
 #include <avr/io.h>
 
 /**
- * @brief uses value from adc to make pwm signal of corresponding duty cycle
- * 
- * @param[in] delay_time Delay in Micro seconds
+ * Function Definitions
+ */
+
+/**
+ * @brief Initialize Timer1 registers 
  * 
  */
-void pwm_init();
+void InitTimer();
+
+/**
+ * @brief Produce duty cycle in PWM according to i/p ADC value
+ * 
+ * @param[in] temp The ADC value from activity2
+ */
+void activity3_PWM(uint16_t temp);
 
 #endif /* __ACTIVITY_3_H__ */
