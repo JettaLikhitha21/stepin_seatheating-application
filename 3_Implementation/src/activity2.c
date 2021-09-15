9cbea8d213523b9edb37c95d6f04102aaec5f6a4
JettaLikhitha21 <89720767+JettaLikhitha21@users.noreply.github.com>
2021-09-15 10:38:48 +0530

    Update activity2.c

--- 3_Implementation/src/activity2.c
+++ 3_Implementation/src/activity2.c
@@ -1,29 +1,38 @@
+/**
+ * @file activity2.c
+ * @author jettalikhitha 
+ * @brief Functions to get the ADC values 
+ * @version 0.1
+ * @date 2021-09-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include "activity2.h"
 
-/**
- * @brief initializes registers
- * 
- */
-void adc_init()
+void InitADC()
 {
-    ADMUX|=(1<<REFS0);
-    ADCSRA=(1<<ADEN)|(7<<ADPS0);
+    ADMUX=(1<<REFS0);
+    ADCSRA= (1<<ADEN)|(7<<ADPS0);
 }
 
-/**
- * @brief converts input from potentiometer to digital value
- * 
- * @param ch 
- * @return uint16_t 
- */
-
-uint16_t adc_read(uint8_t ch)
+uint16_t ReadADC(uint8_t ch)
 {
     ADMUX&=0xf8;
     ch=ch&0b00000111;
     ADMUX|=ch;
+
     ADCSRA|=(1<<ADSC);
-    while(!(ADCSRA&(1<<ADIF)));
+    while(!(ADCSRA & (1<<ADIF)));
     ADCSRA|=(1<<ADIF);
-    return (ADC);
+    return(ADC);
+}
+
+uint16_t activity2_GetADC(void)
+{
+      InitADC();
+      uint16_t temp;
+      temp=ReadADC(0);
+      return temp;
 }
