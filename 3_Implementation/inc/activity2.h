1f75738759a7da7da218ac39c11b7dc495f0d17f
JettaLikhitha21 <89720767+JettaLikhitha21@users.noreply.github.com>
2021-09-15 10:42:01 +0530

    Update activity2.h

--- 3_Implementation/inc/activity2.h
+++ 3_Implementation/inc/activity2.h
@@ -2,29 +2,44 @@
 #define __ACTIVITY_2_H__
 /**
  * @file activity2.h
- * @author anoop
- * @brief activity2
+ * @author jettalikhitha
+ * @brief Header file for activity2
  * @version 0.1
  * @date 2021-04-27
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+
+/**
+ * Include files
+ */ 
+#include <util/delay.h>
 #include <avr/io.h>
 
 /**
- * @brief initializes registers
- * 
+ * Function Definitions
  */
-void adc_init();
 
 /**
- * @brief converts input from potentiometer to digital value
+ * @brief Initialize the registers for ADC setup
  * 
- * @param ch 
- * @return uint16_t 
  */
-uint16_t adc_read(uint8_t ch);
+void InitADC();
+
+/**
+ * @brief Gives the ADC values for the selected channel
+ * 
+ * @param[in] ch Channel number from 0-7
+ * @return uint16_t ADC value
+ */
+uint16_t ReadADC(uint8_t ch);
+
+/**
+ * @brief Return the ADC value to main function
+ * 
+ * @return uint16_t The ADC value
+ */
+uint16_t activity2_GetADC(void);
 
 #endif /* __ACTIVITY_2_H__ */
