86ef1fda5eed03ecff6fd0578f0aa6030403980c
JettaLikhitha21 <89720767+JettaLikhitha21@users.noreply.github.com>
2021-09-15 10:41:03 +0530

    Update activity1.h

--- 3_Implementation/inc/activity1.h
+++ 3_Implementation/inc/activity1.h
@@ -2,29 +2,59 @@
 #define __ACTIVITY_1_H__
 /**
  * @file activity1.h
- * @author anoop
- * @brief activity1
+ * @author jettalikhitha
+ * @brief Header file for activity1
  * @version 0.1
- * @date 2021-04-27
+ * @date 2021-09-14
  * 
  * @copyright Copyright (c) 2021
  * 
  */
+
+
+/**
+ * Macro Definitions
+ */
+
 #define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+#define LED_PORT (PORTD)    /**< LED Port Number */
+#define LED_PIN  (PORTD2)   /**< LED Pin number  */
+#define BUTTON_SENSOR  (PORTD0)   /**< Port for Button Sensor  */
+#define TEMP_SENSOR  (PORTD1)   /**< Port for Temperature Sensor   */
+
+/**
+ * Include files
+ */ 
+#include <util/delay.h>
 #include <avr/io.h>
 
-/**
- * @brief initialise registers
- * 
- */
-void peripheral_init();
 
 /**
- * @brief && 2 buttons and proceed if both are on
- * 
- * @return uint16_t 
+ * Function Definitions
  */
-uint16_t buttons();
 
+/**
+ * @brief Initialize all the Peripherals and pin configurations
+ * 
+ */
+void peripheral_init(void);
+
+/**
+ * @brief Function to turn LED on
+ * 
+ */
+void TurnLED_ON();
+
+/**
+ * @brief Function to turn LED off
+ * 
+ */
+void TurnLED_OFF();
+
+/**
+ * @brief Activity 1 to Turn ON LED if switches for Button sensor and Heator sensor are pressed 
+ * 
+ */
+int activity1_LED(void);
 
 #endif /* __ACTIVITY_1_H__ */
